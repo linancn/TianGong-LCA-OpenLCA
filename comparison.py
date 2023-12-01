f8d42c682f6309cc7b6dd71107206f3b55f8e123
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2023-12-01 15:19:32 +0800

    update for Traditional Chinese (Taiwan) correction

--- comparison.py
+++ comparison.py
@@ -7,22 +7,48 @@ def read_file(file_path):
 # 用于解析键值对的函数
 def parse_key_values(lines):
     return dict(line.strip().split("=") for line in lines if line.strip())
+    # key_values = {}
+    # for line in lines:
+    #     if line.strip():
+    #         parts = line.strip().split("=")
+    #         if len(parts) == 2:
+    #             key_values[parts[0]] = parts[1]
+    #         else:
+    #             print(f"Skipping invalid line: {line}")
+    # return key_values
 
-
-# 读取文件
-file1_lines = read_file("messages_zh_cn.properties")
-file2_lines = read_file("messages.properties")
+# 读取messages文件
+file1_lines = read_file("properties-raw/messages_zh_tw.properties")#properties-raw/bundle_zh_tw.properties #properties-raw/messages_zh_tw.properties
+file2_lines = read_file("properties-raw/messages.properties")#properties-raw/bundle.properties #properties-raw/messages.properties
 
 # 解析键值对
 file1_key_values = parse_key_values(file1_lines)
 file2_key_values = parse_key_values(file2_lines)
 
 # 找出只在文件1中的键值对
-unique_to_file1 = {
-    k: v for k, v in file1_key_values.items() if k not in file2_key_values
+unique_to_file2 = {
+    k: v for k, v in file2_key_values.items() if k not in file1_key_values
 }
 
-# 写入新文件
-with open("new.properties", "w", encoding="utf-8") as new_file:
-    for key, value in unique_to_file1.items():
+# 写入新messages文件
+with open("properties-raw/messages_zh_tw_new.properties", "w", encoding="utf-8") as new_file:
+    for key, value in unique_to_file2.items():
         new_file.write(f"{key}={value}\n")
+
+# # 读取bundle文件
+# file1_lines = read_file("properties-raw/bundle_zh_tw.properties")#properties-raw/bundle_zh_tw.properties #properties-raw/messages_zh_tw.properties
+# file2_lines = read_file("properties-raw/bundle.properties")#properties-raw/bundle.properties #properties-raw/messages.properties
+
+# # 解析键值对
+# file1_key_values = parse_key_values(file1_lines)
+# file2_key_values = parse_key_values(file2_lines)
+
+# # 找出只在文件2中的键值对
+# unique_to_file2 = {
+#     k: v for k, v in file2_key_values.items() if k not in file1_key_values
+# }
+
+# # 写入新文件
+# with open("properties-raw/bundle_zh_tw_new.properties", "w", encoding="utf-8") as new_file:
+#     for key, value in unique_to_file2.items():
+#         new_file.write(f"{key}={value}\n")
--- decode.py
+++ decode.py
@@ -1,15 +1,33 @@
-with open("properties/messages_zh.properties","r",encoding="utf-8") as f:
+#message_zh_cn
+with open("properties-raw/messages_zh_cn.properties","r",encoding="utf-8") as f:
     content = f.read()
 
 #解码
-with open("output_messages.txt","w",encoding="utf-8") as f:
+with open("properties-txt/output_messages_zh_cn.txt","w",encoding="utf-8") as f:
     f.write(content.encode("utf-8").decode("unicode_escape"))
     
-
-with open("properties/bundle_zh.properties","r",encoding="utf-8") as f:
+#bundle_zh_cn
+with open("properties-raw/bundle_zh_cn.properties","r",encoding="utf-8") as f:
     content = f.read()
 
 #解码
-with open("output_bundle.txt","w",encoding="utf-8") as f:
+with open("properties-txt/output_bundle_zh_cn.txt","w",encoding="utf-8") as f:
+    f.write(content.encode("utf-8").decode("unicode_escape"))
+
+#message_zh_tw
+with open("properties-raw/messages_zh_tw.properties","r",encoding="utf-8") as f:
+    content = f.read()
+
+#解码
+with open("properties-txt/output_messages_zh_tw.txt","w",encoding="utf-8") as f:
+    f.write(content.encode("utf-8").decode("unicode_escape"))
+
+
+#bundle_zh_tw
+with open("properties-raw/bundle_zh_tw.properties","r",encoding="utf-8") as f:
+    content = f.read()
+
+#解码
+with open("properties-txt/output_bundle_zh_tw.txt","w",encoding="utf-8") as f:
     f.write(content.encode("utf-8").decode("unicode_escape"))
 
