1afeb56950c03fd5987c8d4a7a27f60b6f0b8506
Huimin Chang <huiminchang@mail.bnu.edu.cn>
2023-12-04 11:13:17 +0800

    update tradational Chinese (Taiwan) language file

--- comparison.py
+++ comparison.py
@@ -6,20 +6,23 @@ def read_file(file_path):
 
 # 用于解析键值对的函数
 def parse_key_values(lines):
-    return dict(line.strip().split("=") for line in lines if line.strip())
-    # key_values = {}
-    # for line in lines:
-    #     if line.strip():
-    #         parts = line.strip().split("=")
-    #         if len(parts) == 2:
-    #             key_values[parts[0]] = parts[1]
-    #         else:
-    #             print(f"Skipping invalid line: {line}")
-    # return key_values
+    # return dict(line.strip().split("=") for line in lines if line.strip())
+    key_values = {}
+    for line in lines:
+        if line.strip():
+            parts = line.strip().split("=")
+            if len(parts) == 2:
+                key_values[parts[0]] = parts[1]
+            else:
+                print(f"Skipping invalid line: {line}")
+    return key_values
 
 # 读取messages文件
-file1_lines = read_file("properties-raw/messages_zh_tw.properties")#properties-raw/bundle_zh_tw.properties #properties-raw/messages_zh_tw.properties
-file2_lines = read_file("properties-raw/messages.properties")#properties-raw/bundle.properties #properties-raw/messages.properties
+# file1_lines = read_file("properties-raw/messages_zh.properties")#properties-raw/bundle_zh_tw.properties #properties-raw/messages_zh_tw.properties
+# file2_lines = read_file("properties-raw/messages.properties")#properties-raw/bundle.properties #properties-raw/messages.properties
+
+file1_lines = read_file("properties-raw/messages.properties")#properties-raw/bundle_zh_tw.properties #properties-raw/messages_zh_tw.properties
+file2_lines = read_file("properties-raw/messages_zh.properties")#properties-raw/bundle.properties #properties-raw/messages.properties
 
 # 解析键值对
 file1_key_values = parse_key_values(file1_lines)
@@ -31,7 +34,7 @@ unique_to_file2 = {
 }
 
 # 写入新messages文件
-with open("properties-raw/messages_zh_tw_new.properties", "w", encoding="utf-8") as new_file:
+with open("properties-raw/messages_zh_new_del.properties", "w", encoding="utf-8") as new_file:
     for key, value in unique_to_file2.items():
         new_file.write(f"{key}={value}\n")
 
--- decode.py
+++ decode.py
@@ -1,33 +1,33 @@
-#message_zh_cn
-with open("properties-raw/messages_zh_cn.properties","r",encoding="utf-8") as f:
-    content = f.read()
+# #message_zh_cn
+# with open("properties-raw/messages_zh_cn.properties","r",encoding="utf-8") as f:
+#     content = f.read()
 
-#解码
-with open("properties-txt/output_messages_zh_cn.txt","w",encoding="utf-8") as f:
-    f.write(content.encode("utf-8").decode("unicode_escape"))
+# #解码
+# with open("properties-txt/output_messages_zh_cn.txt","w",encoding="utf-8") as f:
+#     f.write(content.encode("utf-8").decode("unicode_escape"))
     
-#bundle_zh_cn
-with open("properties-raw/bundle_zh_cn.properties","r",encoding="utf-8") as f:
-    content = f.read()
+# #bundle_zh_cn
+# with open("properties-raw/bundle_zh_cn.properties","r",encoding="utf-8") as f:
+#     content = f.read()
 
-#解码
-with open("properties-txt/output_bundle_zh_cn.txt","w",encoding="utf-8") as f:
-    f.write(content.encode("utf-8").decode("unicode_escape"))
+# #解码
+# with open("properties-txt/output_bundle_zh_cn.txt","w",encoding="utf-8") as f:
+#     f.write(content.encode("utf-8").decode("unicode_escape"))
 
 #message_zh_tw
 with open("properties-raw/messages_zh.properties","r",encoding="utf-8") as f:
     content = f.read()
 
 #解码
-with open("properties-txt/output_messages_zh_tw.txt","w",encoding="utf-8") as f:
+with open("properties-txt/output_messages_zh_tw_1.txt","w",encoding="utf-8") as f:
     f.write(content.encode("utf-8").decode("unicode_escape"))
 
 
-#bundle_zh_tw
-with open("properties-raw/bundle_zh.properties","r",encoding="utf-8") as f:
-    content = f.read()
+# #bundle_zh_tw
+# with open("properties-raw/bundle_zh.properties","r",encoding="utf-8") as f:
+#     content = f.read()
 
-#解码
-with open("properties-txt/output_bundle_zh_tw.txt","w",encoding="utf-8") as f:
-    f.write(content.encode("utf-8").decode("unicode_escape"))
+# #解码
+# with open("properties-txt/output_bundle_zh_tw.txt","w",encoding="utf-8") as f:
+#     f.write(content.encode("utf-8").decode("unicode_escape"))
 
